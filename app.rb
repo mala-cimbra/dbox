af324d8c9d5f33a0b3c568769d65f4f3bb2f36ab
Ioma Taani <iomataani@users.noreply.github.com>
2016-07-07 15:27:15 +0200

    Commentato codice

--- app.rb
+++ app.rb
@@ -9,14 +9,27 @@ require 'json'
 require 'base64'
 require 'digest'
 
+#-----------------------------
+#
+# Settaggi per Sinatra
+#
+#----------------------------
+
 set :server, %w[thin webrick]
 set :bind, '0.0.0.0'
 set :port, 8080
 
 #------------------------------
+#
+# Database SQLite al momento
+#
+# -----------------------------
+
 db = SQLite3::Database.open("database.db")
 
 #------------------------------
+# HTTP Login
+#
 # Ricordati di toglierlo quando
 # va in produzione
 #------------------------------
@@ -27,67 +40,107 @@ end
 
 #------------------------------
 
+# index
 get '/' do
     erb :index
 end
 
+# funzione dummy POST per postare messaggi - TODO
 post '/message' do
 
 end
 
+# Pagina download
 get '/downloads' do
+    # prende la lista dei file da database e lo porta in un array
+    # visto che funziona a righe avrebbe dei array annidati,
+    # qui il flatten per incicciottare [[row][row][row][row][row]]
+    # tutto in un unico array [row, row, row, row]
+    # inutile ciclare per un dato solo, se si avranno bisogno di più dati nella riga
+    # ci si penserà
+    #
+    # poi si spedisce @list alla pagina downloads.erb
     @list = db.execute("SELECT filename FROM files;").flatten
-=begin
-    @list = Dir.glob("./public/uploads/*.*").map do |file|
-        file.split('/').last
-    end
-=end
     erb :downloads
 end
 
 get '/downloads/:filename' do |filename|
+    # se il nome del file esiste
+    # TODO: fargli fare un controllo anche sul db
+    # se per caso esiste il file, ma non sul db, boh,
+    # magari gli faccio scrivere qualche file di log
+    # è comunque una cosa che non dovrebbe succedere,
+    # ma la sfiga è sempre tanta
     if File.exist?("./public/uploads/#{filename}")
+        # tira fuori il numero di dl del file per aumentare il contatore
         dl_count = db.execute("SELECT dl_number from files WHERE filename = '#{filename}';")
-        new_dl_count = dl_count.flatten[0].to_i + 1
+        new_dl_count = dl_count.flatten[0].to_i + 1 #lo flatta per il discorso sopra e aggiunge 1
+        # aggiorna il db, sql a nastro
         db.execute("UPDATE files SET dl_number=#{new_dl_count}, last_dl_ip='#{request.ip}', last_dl_date='#{Time.now}' WHERE filename='#{filename}';")
+        # spedisce il file
         send_file("./public/uploads/#{filename}", :filename => filename, :disposition => :attachment, :type => 'application/octet-stream')
     else
+        # altrimenti torna indrio ai download
         redirect to('/downloads')
     end
 end
 
+# mostra la pagina di upload
 get '/upload' do
     erb :upload
 end
 
+# http post per spedire il file
 post '/upload' do
+    # semplice check sull'input, se qualcuno spara zero file, se qualcuno uppa un file con
+    # lo stesso nome già presente
     if params['file'] == nil || params['file'] == "" || File.exist?("./public/uploads/#{params['file'][:filename]}")
+        # redireziona ad upload
+        # TODO magari mostrare un messaggio di errore
         redirect to('/upload')
     else
-        tempfile = params['file'][:tempfile]
-        filename = params['file'][:filename]
-        path = "./public/uploads/#{filename}"
+        # qua c'è un po' di magia, la prendo per buona
+        tempfile = params['file'][:tempfile] # file temporaneo uploadato, penso vada in /tmp
+        filename = params['file'][:filename] # file scritto
+        path = "./public/uploads/#{filename}" # percorso di destinazione
+        # scrivi
         File.open(path, "wb") do |f|
             f.write(tempfile.read)
         end
-        shadigest = Digest::SHA256.hexdigest(File.read(path))
-        delete_password = params['password']
+        shadigest = Digest::SHA256.hexdigest(File.read(path)) # calcola sommahash
+        delete_password = params['password'] # tirami fuori la password
+        # aggiungi la cosa al db
         db.execute("INSERT INTO files VALUES(NULL, '#{filename}', '#{path}', '#{shadigest}', '#{request.ip}', '#{Time.now}', 0, NULL, NULL, '#{delete_password}');")
     end
-    redirect to('/downloads')
+    redirect to('/downloads') # mostrami poi quello che hai buttato su
 end
 
+# funzione di eliminazione
+# TODO creare la pagina di protezione con la password
+# devo pensarla bene
 get '/delete/:filename' do |filename|
+    #se il file esiste
     if File.exist?("./public/uploads/#{filename}")
+        # prima togli dal db, così se scazza almeno il frontend funziona
+        # cerca la riga con quel nome file
+        # TODO aggiungere più campi di ricerca (somma hash?, boh)
         db_file = db.execute("SELECT * FROM files where filename = '#{filename}';")
+        # elimina la riga dal db
         db.execute("DELETE FROM files WHERE Id = #{db_file[0][0]};")
+        # elimina il file
         File.delete("./public/uploads/#{filename}")
-        redirect to ('/downloads')
+        redirect to ('/downloads') # vai ai downloads
     else
-        redirect to ('/downloads')
+        redirect to ('/downloads') # vai ai downloads
     end
 end
 
+# se non sai dove andare
+# (errore 404)
+# redireziona all'index
+# Non serve fare pagine che mostrano l'errore
+# perché deve funzionare da captive portal
+# ma si può ampliare nel caso
 not_found do
     redirect to('/')
 end

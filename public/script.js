26730489aa591ebdf092d6ca7ac303dc35547abe
Ioma Taani <iomataani@users.noreply.github.com>
2016-07-07 14:29:17 +0200

    Aggiunto il toggle visibilità.

---
+++ public/script.js
@@ -0,0 +1,12 @@
+function toggle_visibility(id)
+{
+    var vis = document.getElementById(id);
+    if (vis.style.display == 'block')
+    {
+        vis.style.display = 'none';
+    }
+    else
+    {
+        vis.style.display = 'block';
+    }
+}

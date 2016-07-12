72ab8b8ab54ed1fa1153715126dd34902db31e9e
Ioma Taani <iomataani@users.noreply.github.com>
2016-07-12 10:07:23 +0200

    Informazioni sul file audio

--- app.rb
+++ app.rb
@@ -179,10 +179,16 @@ post '/delete/:filename/confirm' do |filename|
 end
 
 get '/info/:filename' do |filename|
-    db_file = db.execute("SELECT * FROM files WHERE filename = '#{filename}';")
-    debug("db_file", db_file)
-    if File.exist?("./public/uploads/#{filename}") && !db_file[0].empty?
-        
+    @db_file = db.execute("SELECT * FROM files WHERE filename = '#{filename}';")
+    debug("db_file", @db_file)
+    if File.exist?(@db_file[0][2]) && !@db_file[0].empty?
+        @arr_data = Array.new
+        TagLib::FileRef.open(@db_file[0][2]) do |audio|
+            tag = audio.tag
+            prop = audio.audio_properties
+            string_durata = "#{prop.length / 60}:#{prop.length % 60}"
+            @arr_data << tag.artist << tag.title << tag.album << string_durata << prop.bitrate
+        end
         erb :info
     else
         redirect to ('/downloads') # vai ai downloads

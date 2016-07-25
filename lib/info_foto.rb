3f591b4590697abd88e39ad9d37b74081267d88c
Ioma Taani <iomataani@users.noreply.github.com>
2016-07-25 16:46:50 +0200

    funzione per le info sulle immagini, da migliorare magari

--- lib/info_foto.rb
+++ lib/info_foto.rb
@@ -1,1 +1,12 @@
 #!/usr/bin/env ruby
+
+# gestito da mini_exiftool
+
+def info_foto(path)
+    foto_data = Hash.new
+    foto = MiniExiftool.new(path)
+    
+    foto_data = {size: foto.imagesize, model: foto.model, createdate: foto.createdate}
+
+    JSON.generate(foto_data)
+end

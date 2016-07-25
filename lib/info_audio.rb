53f3edf0189a7df7f1db8a4408e30e8462bac745
Ioma Taani <iomataani@users.noreply.github.com>
2016-07-25 13:03:26 +0200

    info_audio fa solo da analisi dopo l'upload

--- lib/info_audio.rb
+++ lib/info_audio.rb
@@ -1,25 +1,21 @@
 #!/usr/bin/env ruby
 
-def info_audio(filename, path, filetype)
-    audio_data = Array.new
+# gestito da taglib-ruby
+
+def info_audio(path)
+
+    audio_data = Hash.new # hash contenente di dati da convertire in json
     
     TagLib::FileRef.open(path) do |audio|
         tag = audio.tag
         prop = audio.audio_properties
-        string_durata = "#{prop.length / 60}:#{prop.length % 60}"
-        audio_data << tag.artist << tag.title << tag.album << string_durata << prop.bitrate
+        minuti = prop.length / 60
+        secondi = sprintf("%0.2d", prop.length % 60)
+        string_durata = "#{minuti}:#{secondi}"
+        
+        # popola l'hash con i dati
+        audio_data = {artist: tag.artist, title: tag.title, album: tag.album, length: string_durata, bitrate: prop.bitrate}
     end
-    
-"""<ul>
-    <li><strong>Artista: </strong>#{audio_data[0]}</li>
-    <li><strong>Titolo: </strong>#{audio_data[1]}</li>
-    <li><strong>Album: </strong>#{audio_data[2]}</li>
-    <li><strong>Durata: </strong>#{audio_data[3]}</li>
-    <li><strong>Bitrate: </strong>#{audio_data[4]} kbps</li>
-</ul>
-<h3>Anteprima</h3>
-<div align=\"center\"><audio controls>
-    <source src=\"/downloads/#{filename}\" type=\"#{filetype}\">
-Il tuo browser è vecchio e non supporta il tag audio. Aggiornati!
-</audio></div>"""
+
+    JSON.generate(audio_data) # genera JSON
 end
